61947218f46828366da407851e37e682c1835a6e
islommashanlo <islom.mashanlo@gmail.com>
2020-09-21 23:03:17 -0400

    imitated

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,6 +1,19 @@
 require 'faker'
 
 class PokemonsController < ApplicationController
+
+  def index
+    pokemons = Pokemon.all
+
+    render json: pokemons
+  end
+
+  def show
+    pokemon =  Pokemon.find(params[:id])
+
+    render json: pokemon
+  end
+
   def create
     unless pokemon_params[:trainer_id].nil?
       default = {}
